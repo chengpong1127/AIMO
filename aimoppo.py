0ddc0f1c35e233791a26f3029770590e1a86c418
Chengpong1127 <chengpong1127@gmail.com>
2024-05-13 22:55:20 +0800

    init

---
+++ aimoppo.py
@@ -0,0 +1,206 @@
+
+import yaml
+import re
+import numpy as np
+import torch
+from datasets import Dataset
+from peft import LoraConfig
+from transformers import AutoTokenizer, load_tool, BitsAndBytesConfig
+from tqdm.auto import tqdm
+
+from trl import AutoModelForCausalLMWithValueHead, PPOConfig, PPOTrainer, TextEnvironment
+from dataclasses import dataclass
+from typing import Optional
+from dotenv import load_dotenv
+import bitsandbytes as bnb
+
+load_dotenv()
+
+@dataclass
+class TrainerConfig:
+    model_name: str
+    epochs: int
+    train_batch_size: int
+    train_mini_batch_size: int
+    test_batch_size: int
+    gradient_accumulation_steps: int
+    
+    save_dir: str
+    
+
+class AIMOPPOTrainer:
+    
+    
+    
+    def __init__(self, config: TrainerConfig, train_dataset: Dataset, test_dataset: Optional[Dataset] = None):
+        # read config file
+        self.config = config
+        
+        bnb_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_compute_dtype=torch.bfloat16,
+        )
+        lora_config = LoraConfig(
+            r=16,
+            lora_alpha=32,
+            lora_dropout=0.05,
+            bias="none",
+            task_type="CAUSAL_LM",
+        )
+        self.model = AutoModelForCausalLMWithValueHead.from_pretrained(
+            self.config.model_name,
+            peft_config=lora_config,
+            quantization_config=bnb_config
+        )
+        
+        
+        self.tokenizer = AutoTokenizer.from_pretrained(self.config.model_name)
+        self.tokenizer.pad_token = self.tokenizer.eos_token
+        prompt = """
+Let's think step by step to ensure the answer is correct. Submit your answer like "Result = 8 <submit>".
+Problem:"""
+        
+        generation_kwargs = {
+            "max_new_tokens": 512,
+            "min_length": -1,
+            "top_k": 0.0,
+            "top_p": 1.0,
+            "do_sample": True,
+            "pad_token_id": self.tokenizer.eos_token_id,
+            "eos_token_id": -1,
+        }
+        self.text_env = TextEnvironment(
+            self.model,
+            self.tokenizer,
+            [load_tool("lvwerra/python-interpreter")],
+            self._exact_match_reward,
+            prompt,
+            max_turns=1,
+            generation_kwargs=generation_kwargs,
+        )
+        ppo_config = PPOConfig(
+            batch_size=self.config.train_batch_size,
+            mini_batch_size=self.config.train_mini_batch_size,
+            ppo_epochs=self.config.epochs,
+            gradient_accumulation_steps = self.config.gradient_accumulation_steps,
+            remove_unused_columns=False,
+            optimize_cuda_cache=True,
+            log_with="wandb",
+            tracker_project_name="AIMO",
+            project_kwargs = {
+                "project_dir": self.config.save_dir,
+                "automatic_checkpoint_naming": True,
+                "total_limit": 5,
+            }
+        )
+        adam = bnb.optim.Adam8bit(self.model.parameters())
+        split = train_dataset.train_test_split(test_size=0.1)
+        _train_dataset, _val_dataset = split["train"], split["test"]
+        self.ppo_trainer: PPOTrainer = PPOTrainer(config=ppo_config, model=self.model, tokenizer=self.tokenizer, dataset=_train_dataset, optimizer=adam)
+        self.model = self.ppo_trainer.model
+        self.train_dataloader = self.ppo_trainer.dataloader
+        self.val_dataloader = self.ppo_trainer.prepare_dataloader(_val_dataset)
+        
+        self.accelerator = self.ppo_trainer.accelerator
+        
+        
+        
+    def _train_step(self, batch, text_env: TextEnvironment, ppo_trainer: PPOTrainer):
+        queries, responses, masks, rewards, _ = text_env.run(batch["query"], answers=batch["answer"])
+        train_stats = ppo_trainer.step(queries, responses, rewards)
+        return train_stats, queries, responses, rewards
+    
+    def _evaluate(self, test_dataloader, text_env: TextEnvironment, ppo_trainer: PPOTrainer):
+        test_rewards = []
+        for test_batch in test_dataloader:
+            _, _, _, rewards, histories = text_env.run(test_batch["query"], answers=test_batch["answer"])
+            test_rewards.extend(rewards)
+        test_rewards = ppo_trainer.accelerator.gather_for_metrics(
+            torch.stack(test_rewards).to(ppo_trainer.accelerator.device)
+        )
+        return test_rewards.mean()
+    
+    def _exact_match_reward(self, responses, answers):
+        """Reward if generated response contains correct answer."""
+        rewards = []
+        for response, answer in zip(responses, answers):
+            reward = 0.0
+            predicted_number = self._get_answer(response)
+            if predicted_number is not None:
+                if np.abs(predicted_number - float(answer)) < 0.1:
+                    reward += 1.0
+            rewards.append(torch.tensor(reward))
+        return rewards
+    
+    def _get_answer(self, response):
+        try:
+            pattern = r"Result\s*=\s*(-?\d+(?:\.\d+)?)\s*<submit>"
+            match_pattern = re.findall(pattern, response)
+            if match_pattern:
+                return float(match_pattern[0])
+            else:
+                return 0
+        except Exception:
+            return 0
+    
+    
+    def train(self):
+        self._train_inner_loop(self.ppo_trainer, self.text_env, self.tokenizer, self.config.epochs, self.train_dataloader, self.val_dataloader)
+        reward_mean_test = self._evaluate(self.val_dataloader, self.text_env, self.ppo_trainer)
+        self._log({"val_reward_mean": reward_mean_test})
+                
+    def _train_inner_loop(self, ppo_trainer: PPOTrainer, text_env: TextEnvironment, tokenizer, epochs, train_dataloader, val_dataloader):
+        
+        progress_bar = tqdm(range(epochs * len(train_dataloader)))
+        for _ in range(epochs):
+            for step, batch in enumerate(train_dataloader):
+                if step == 0 and self.accelerator.is_local_main_process:
+                    reward_mean_test = self._evaluate(val_dataloader, text_env, ppo_trainer)
+                    self._log({"val_reward_mean": reward_mean_test})
+                    self._save_checkpoint()
+
+                train_stats, _, responses, rewards = self._train_step(batch, text_env, ppo_trainer)
+                responses = [tokenizer.decode(response) for response in responses]
+                
+                self._log_stats(ppo_trainer, batch["query"], responses, batch["answer"], rewards, train_stats)
+                progress_bar.update(1)
+                
+                
+                
+    def _log_stats(self, ppo_trainer: PPOTrainer, queries: list, responses: list, answers: list, rewards, train_stats: dict):
+        texts = {
+            "query": queries,
+            "response": responses,
+            "answer": answers,
+        }
+        ppo_trainer.log_stats(train_stats, texts, rewards, columns_to_log=["query", "response", "answer"])
+        
+    def _log(self, data: dict):
+        self.accelerator.log(data, self.ppo_trainer.current_step)
+        
+    def _save_checkpoint(self):
+        self.accelerator.save_state()
+    
+    def load_checkpoint(self, checkpoint_dir: str):
+        self.accelerator.load_state(checkpoint_dir)
+        
+    def save_model(self, output_dir: str):
+        self.model.save_pretrained(output_dir)
+        
+    def infer(self, math_problem: str):
+        _, _, _, _, histories = self.text_env.run([math_problem], answers=[0])
+        answer = self._get_answer(histories[0].text)
+        return answer
+    
+    def generate_aimo_submission(self):
+        import aimo
+        env = aimo.make_env()
+        iter_test = env.iter_test()
+        for test, sample_submission in iter_test:
+            sample_submission['answer'] = self.infer(test['problem'])
+            env.predict(sample_submission)
+            print(test)
+            print(sample_submission, '\n')
+        
