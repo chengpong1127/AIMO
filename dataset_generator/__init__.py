5fa08a2943e93f93c3402634367462f640d00193
Chengpong1127 <chengpong1127@gmail.com>
2024-07-02 17:02:36 +0000

    complete copy code and example

--- dataset_generator/__init__.py
+++ dataset_generator/__init__.py
@@ -1,2 +1,2 @@
 from .stage2 import generate_completion_dataset
-from .stage3 import generate_corrective_dataset, generate_kto_dataset
+from .stage3 import generate_corrective_dataset, generate_kto_dataset, generate_copy_dataset
